826320ffa369b621995beb8d9cf7a7bdecaa6d16
Vera Clara Bastos de Sá Sá <blbdesa@hotmail.com>
2022-04-04 15:45:55 -0300

    First Commit

---
+++ POO/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Aluno a = new Aluno("Matutino", "Bryan", "bryan@udemy.com", "12345");
+            Zelador z = new Zelador("Daniel", "daniel@udemy.com", "123456");
+            List<Usuario> usuarios = new List<Usuario>();
+            usuarios.Add(a);
+            usuarios.Add(z);
+            a.Exibir();*/
+
+            IDrawable shape = new Line();
+            shape.Draw();
+
+            Console.ReadLine();
+        }
+    }
+
+     abstract class Drawable
+    {
+        public int size;
+        public int color;
+        public abstract void Draw();
+
+        public abstract float Area();
+
+    }
+
+    interface IDrawable
+    {
+
+        void Draw();
+        float Area();
+    }
+
+    class Line : IDrawable
+    {
+        public float Area()
+        {
+            return 20;
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine("- Linha -");
+        }
+    }
+
+    class Quad : IDrawable
+    {
+        public float Area()
+        {
+            return 2;
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine("- Quad -");
+        }
+    }
+
+    class Rect : IDrawable
+    {
+        public float Area()
+        {
+            return 10;
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine("- Rect -");
+        }
+    }
+}
---
+++ POO/Usuario.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    internal class Usuario
+    {
+        public string nome;
+        public string email;
+        public string senha;
+
+        public Usuario(string nome, string email, string senha)
+        {
+            this.nome = nome;
+            this.email = email;
+            this.senha = senha;
+        }
+
+        public void Logar()
+        {
+            Console.WriteLine("Logando . . . ");
+        }
+
+        public void Logar(string codigo)
+        {
+            Console.WriteLine("Logando com cod . . .");
+        }
+
+        public void Logar(string email, string senha)
+        {
+            Console.WriteLine("Logando com email e senha . . .");
+        }
+
+        public virtual void Exibir()
+        {
+            Console.WriteLine($"Email: {email}");
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Senha: {senha}");
+        }
+    }
+
+    class Aluno : Usuario
+    {
+        public List<string> turmas = new List<string>();
+        public string turno = "Matutino";
+
+        public Aluno(string turno, string nome, string email, string senha): base(nome, email, senha)
+        {
+            this.turno = turno;
+        }
+        public override void Exibir()
+        {
+            Console.WriteLine("Dados do aluno: ");
+            base.Exibir();
+            Console.WriteLine("Turno: " + turno);
+        }
+    }
+
+    class Zelador : Usuario
+    {
+        public List<string> tarefas = new List<string>();
+
+        public Zelador(string nome, string email, string senha): base(nome, email, senha)
+        {
+            
+        }
+    }
+}
